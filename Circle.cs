059d6be8cb7247d8c6b66981d11dad75fe3e5ff1
Emma <elune.lind@gmail.com>
2023-10-12 14:08:49 +0200

    Some comments

--- Circle.cs
+++ Circle.cs
@@ -5,12 +5,13 @@
         //field variables
         private double _pi;
         private int _radius { get; set; }
+      
 
         //default constructor
         public Circle()
         {
-            _pi = Math.PI; // sets _pi as the mathematically correct pi-value
-            _radius = 0; // _radius = 0 as default, if no value is chosen
+            _pi = Math.PI; // sets _pi as the correct pi-value
+            _radius = 0; // _radius = 0 as default if no value is chosen
         }
 
         // instance constructor with one parameter (int radius)
@@ -24,7 +25,8 @@
         public void GetAreaCircle()
         {
             double area = _radius * _radius * _pi;
-            Console.WriteLine("Arean av en cirkel med " + _radius + " cm i radie är: " + Math.Round(area, 1) + "cm²."); // Math.Round(area,1) avrundar talet till 1 decimal
+            // Math.Round(area,1) round up the number to one decimal
+            Console.WriteLine("Arean av en cirkel med " + _radius + " cm i radie är: " + Math.Round(area, 1) + "cm²."); 
         }
 
         //method to get circumference of a circle
@@ -36,16 +38,16 @@
 
         //method to get area of a sphere
         public void GetAreaSphere()
-        {
-            double area = 4 * _pi * (Math.Pow(_radius, 2)); // Math.Pow() is to calculate the power of, or in this case the square of, _radius.
-            Console.WriteLine("Arean av en sfär med radien " + _radius + "cm är: " + Math.Round(area, 1) + "cm².");
+        {// Math.Pow() is to calculate the power of, or in this case the square of, _radius.
+            double area = 4 * _pi * (Math.Pow(_radius, 2)); 
+            Console.WriteLine("Arean av en sfär med " + _radius + "cm i radie är: " + Math.Round(area, 1) + "cm².");
         }
 
         //method to get volume of a sphere
         public void GetVolumeSphere()
         {
             double volume = 4 * _pi * (Math.Pow(_radius, 3)) / 3;
-            Console.WriteLine("Volymen av en sfär med radien " + _radius + " cm är: " + Math.Round(volume, 1) + "ml.");
+            Console.WriteLine("Volymen av en sfär med " + _radius + " cm i radie är: " + Math.Round(volume, 1) + "ml.");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Labb5_OOP
     {
         static void Main(string[] args)
         {
-            Circle circle1 = new Circle(5); //creates object circle1, gives it int-value 5 (the radius of the circle)
+            // instance of class Circle/ creates object circle1 
+            Circle circle1 = new Circle(5); //int-value 5 in parameter (the radius of the circle)
             circle1.GetAreaCircle(); //calls for GetAreaCircle method and implements it on circle1
 
             Console.WriteLine();
@@ -21,7 +22,7 @@ namespace Labb5_OOP
             circle2.GetAreaSphere();
             Console.WriteLine();
 
-            Triangle triangle1 = new Triangle(5, 7); // triangle1 has the height of 5 (cm) and width of 7 (cm)
+            Triangle triangle1 = new Triangle(5, 7); // triangle1 height: 5 cm, and width: 7 cm
             triangle1.GetAreaTriangle();
         }
     }
